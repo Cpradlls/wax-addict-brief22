5f6ba0fbf5a189a7b35154c3067ad9436c64400b
Pradeilles Chloé <74190557+Cpradlls@users.noreply.github.com>
2020-12-04 13:54:30 +0100

    ajout produits

--- back-office--template.php
+++ back-office--template.php
@@ -3,10 +3,12 @@
   @date_default_timezone_set("Europe/Paris"); // fuseau horaire
   @setlocale(LC_TIME, "fr_FR.utf8","fra"); // jours et mois en français
   $dateDuJour = strftime("%A %d %B %Y");
+  $product = file_get_contents("conf/products.json");
+  $productArray = json_decode($product, JSON_OBJECT_AS_ARRAY);
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="fr">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,6 +22,20 @@
   <main>
     <h2>Bienvenue dans votre espace privé</h2>
     <p>Voici les produits disponibles en ce <?php echo $dateDuJour; ?> :</p>
+    <?php
+    foreach($productArray as $key => $value){
+      echo "<div id='conteneur'>"
+          . "<img id='image' src=' ". $value['image'] ." ' alt='Image de la plante'>"
+          . "<ul>"
+          . "<li>Nom: " . $value['nom'] . "</li>"
+          . "<li>Longueur: " . $value['longueur'] . "</li>"
+          . "<li>Prix: " . $value['prix'] . "</li>"
+          . "<li>Nom alternatif: " . $value['nom alternatif'] . "</li>"
+          . "</ul>"
+          . "<br>"
+          . "</div>";
+    }
+    ?>
   </main>
   <footer><a href="logout.php">déconnexion</a></footer>
 </body>
